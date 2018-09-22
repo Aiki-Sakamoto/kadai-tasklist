1a764150657e999a51658fc32c70f891569683ff
aiki.sakamoto <sakamoto.a1234+GitHub@gmail.com>
2018-09-22 18:55:12 +0900

    Modify models add Timestamp

--- src/models/Task.java
+++ src/models/Task.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,6 +28,12 @@ public class Task {
     @Column(name = "content", length = 100, nullable = false)
     private String content;
 
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
     public Integer getId() {
         return id;
     }
@@ -40,6 +48,22 @@ public class Task {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
 
 }
